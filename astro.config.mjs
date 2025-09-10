640353677d4e7ebcb221f67eed26d804122db0ca
Elizabeth Vazquez <ev2070@nyu.edu>
2025-09-09 21:03:43 -0400

    Update astro.config.mjs to prep for auto deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: "https://feliiizabeth.github.io/j-furniture",
+  base: "/j-furniture/",
 });
