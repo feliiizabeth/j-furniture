be5f12ffec5f97fec759f35629ff3a61efca9a15
feliiizabeth <ev2070@nyu.edu>
2025-09-15 16:48:07 -0400

    Enable use of icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import astroIcon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+  integrations: [astroIcon()],
   site: "https://feliiizabeth.github.io/",
   base: "/j-furniture",
 });
